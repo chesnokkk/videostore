fbcc300ac8bb6e18360968515c43d124024fc988
Roman <nahervihuher@gmail.com>
2021-03-19 00:05:14 +0300

    make main

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
-# Create your views here.
+class HomePage(ListView):
+    model = Course
+    template_name = 'courses/home.html'
+    contex_object_name = 'courses'
+    ordering = ['-id']
+
+    def get_context_data(self, *, object_list=None, **kwards):
+        ctx = super(HomePage, self).get_context_data(**kwards)
+        ctx['title'] = 'Главная страница сайта'
+        return ctx
+    
