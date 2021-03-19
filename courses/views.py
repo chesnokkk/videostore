da39a3bb041ed16801b957883575d1437eff70b1
Roman <nahervihuher@gmail.com>
2021-03-19 23:23:59 +0300

    doing without forms

--- courses/views.py
+++ courses/views.py
@@ -21,8 +21,8 @@ class CourseDetailPage(DetailView):
 
     def get_context_data(self, *, object_list=None, **kwards):
         ctx = super(CourseDetailPage, self).get_context_data(**kwards)
-        ctx['title'] = Course.objects.filter(slug=self.kwargs['slug']).first
-        ctx['lessons'] = Lesson.objects.filter(course=ctx['title']).order_by('number')
+        ctx['title'] = Course.objects.filter(slug=self.kwargs['slug']).first()
+        ctx['lessons'] = Lesson.objects.filter(course=ctx['course']).order_by('number')
 
         return ctx
 
@@ -30,3 +30,15 @@ class CourseDetailPage(DetailView):
 class LessonDetailPage(DetailView):
     model = Course
     template_name = 'courses/lesson-detail.html'
+
+
+    def get_context_data(self, *, object_list=None, **kwards):
+        ctx = super(LessonDetailPage, self).get_context_data(**kwards)
+        course = Course.objects.filter(slug=self.kwargs['slug']).first()
+        lesson = list(Lesson.objects.filter(course=course).filter(slug=self.kwargs['lesson_slug']).values())
+        #print (lesson)
+
+        ctx['title'] = lesson[0]['title']
+        ctx['desc'] = lesson[0]['description']
+        ctx['video'] = lesson[0]['video_url'].split('=')[1]
+        return ctx
